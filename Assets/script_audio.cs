90e8e40b6205b0199896015a7c3289e7ca8d54d3
Tamek <khanh.lh06586@sinhvien.hoasen.edu.vn>
2025-02-11 13:13:20 +0700

    Update: Audio Fly 0.1 (not complete)

--- Assets/script_audio.cs
+++ Assets/script_audio.cs
@@ -2,18 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class script_audio : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject textPrefab;
+    public Transform SpamPoint;
+
     public AudioClip audio;
     private AudioSource audioSource;
+
     public TextAsset lyric;
     private float lines = 0;
     public float volumeaudio = 0.1f;
     private string[] Data_breakdown;
-
+    public Canvas canvas;
+    TextMeshProUGUI[] txt;
+    string[] txtbreak;
     void Start()
     {
         gameObject.AddComponent<AudioSource>();
@@ -28,8 +38,15 @@ public class script_audio : MonoBehaviour
 
     private void Update()
     {
-
+        
         Debug.Log(Data_breakdown[1]);
+        txtbreak = Data_breakdown[1].Split("");
+        for (int i = 0; i < Data_breakdown.Length; i++)
+        {
+
+            Instantiate(textPrefab, SpamPoint.position, SpamPoint.rotation);
+        }
+        
     }
 
     private void breakdown()
